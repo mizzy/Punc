d8950004bf0d84cc48853346b09b23878cd38286
Gosuke Miyashita <gosukenator@gmail.com>
2008-05-06 10:32:34 +0000

    Punc::Util をなくして Punc->context を使うようにした

--- lib/Punc/Slave/Module.pm
+++ lib/Punc/Slave/Module.pm
@@ -2,7 +2,6 @@ package Punc::Slave::Module;
 
 use strict;
 use warnings;
-use Punc::Util;
 use Moose;
 use Module::Pluggable;
 
@@ -35,7 +34,7 @@ sub delegate {
         my $default_for = $module->default_for;
         next unless $default_for;
         my ( $fact ) = keys %$default_for;
-        if ( grep { Punc::Util->fact($fact) =~ /$_/i } @{ $default_for->{$fact} } ) {
+        if ( grep { Punc->context->fact($fact) =~ /$_/i } @{ $default_for->{$fact} } ) {
             $module_to_delegate = $module;
         }
     }
